7de08ce6e543c9fe40512749b452ae21c1923a04
ypages <ypages@student.42.fr>
2023-03-22 20:07:30 +0100

    add - env tester

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: aumarin <aumarin@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/01/07 22:33:15 by aumarin           #+#    #+#              #
-#    Updated: 2023/03/22 19:06:35 by aumarin          ###   ########.fr        #
+#    Updated: 2023/03/22 20:04:41 by ypages           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,7 @@ SRC		= 	minishell.c \
 			src/prompt.c \
 			src/line.c \
 			src/lexer.c \
-			env/env.c \
+			src/env.c \
 			debug/print_utils.c
 OBJ		=	$(SRC:.c=.o)
 C_FLAGS	=	-Wall -Wextra -Werror
--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: aumarin <aumarin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 12:30:11 by aumarin           #+#    #+#             */
-/*   Updated: 2023/03/22 19:04:45 by aumarin          ###   ########.fr       */
+/*   Updated: 2023/03/22 20:05:58 by ypages           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
@@ -14,13 +14,18 @@
 int	main(int argc, char **argv, char **envp)
 {
 	t_tokens	*tokens;
+	t_env		*env;
 	char		*str_line;
 
+	env = NULL;
 	if (!envp || argc > 1)
 		return (1);
 	(void)argv;
 	while (1)
 	{
+		env = check_env_path(envp);
+		if(env->status == NOK)
+			return (printf("[ Error ] - Environment not finded\n")NOK);
 		str_line = ft_prompt();
 		tokens = lexer(str_line);
 		parse(str_line, tokens);
