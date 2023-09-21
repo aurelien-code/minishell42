017267b11223be472b43cc1b20c1c5998edefbf7
aumarin <aumarin@student.42.fr>
2023-09-21 19:06:49 +0200

    fix: compiling error and parsing errors 	- fix: bad variable name resulting in an compilation error 	- fix: parser was misfunctionning without pipe

--- src/errors.c
+++ src/errors.c
@@ -6,7 +6,7 @@
 /*   By: aumarin <aumarin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 15:46:21 by aumarin           #+#    #+#             */
-/*   Updated: 2023/09/20 14:56:10 by aumarin          ###   ########.fr       */
+/*   Updated: 2023/09/21 18:51:05 by aumarin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	*throw_parsing_error(t_lexer *lex, t_tokens *tok, t_cmd *cmd, char *err)
 {
 	if (lex)
-		free(lexer);
+		free(lex);
 	if (tok)
 		free(tok);
 	if (cmd)
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: aumarin <aumarin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 12:00:11 by aumarin           #+#    #+#             */
-/*   Updated: 2023/09/20 18:47:09 by aumarin          ###   ########.fr       */
+/*   Updated: 2023/09/21 19:04:10 by aumarin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,15 +110,17 @@ t_cmd	*parser(t_tokens *tokens)
 	cmds = NULL;
 	while (tokens)
 	{
-		i++;
 		if (tokens->type == T_PIPE)
 		{
 			add_cmd_item(&cmds, process_command(&tmp, i));
 			i = 1;
 		}
+		i++;
 		tokens = tokens->next;
 	}
+	if (tmp->type == T_PIPE)
+		tmp = tmp->next;
 	if (!cmds || tmp)
-		add_cmd_item(&cmds, process_command(&tmp->next, i));
+		add_cmd_item(&cmds, process_command(&tmp, i));
 	return (cmds);
 }
