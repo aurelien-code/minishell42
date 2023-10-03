b180f4ff6372ea30d5e915b6ddb12765ef11c4fd
Arthur Agathe <arthur.agathe.a@gmail.com>
2023-10-03 07:58:43 +0200

    change path checking

--- src/pipe/execute.c
+++ src/pipe/execute.c
@@ -6,56 +6,11 @@
 /*   By: aumarin <aumarin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 17:08:43 by aagathe           #+#    #+#             */
-/*   Updated: 2023/10/03 02:37:04 by aagathe          ###   ########.fr       */
+/*   Updated: 2023/10/03 07:09:43 by aagathe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-int	launch_builtin(t_cmd *cmds, char ***env, char *path)
-{
-	int	ret;
-
-	free(path);
-	ret = 0;
-	if (cmds->is_builtin == 1)
-		ret = (ft_echo(cmds));
-	else if (cmds->is_builtin == 2)
-		ret = (ft_cd(cmds, *env));
-	else if (cmds->is_builtin == 3)
-		ret = (ft_env(cmds, *env));
-	else if (cmds->is_builtin == 4)
-		ret = (ft_export(cmds, env));
-	else if (cmds->is_builtin == 5)
-		ret = (ft_pwd(cmds));
-	else if (cmds->is_builtin == 6)
-		ret = (ft_unset(cmds, env));
-	else if (cmds->is_builtin == 7)
-		ret = (ft_exit(cmds, *env, 1));
-	free_pathes(*env);
-	return (ret);
-}
-
-int	launch_cmd_next(char *path, char **cmd, char **env)
-{
-	char	*error;
-
-	if (path)
-	{
-		execve(path, cmd, env);
-		perror(path);
-		free(path);
-		return (126);
-	}
-	else
-	{
-		error = ft_strjoin(cmd[0], ": command not found");
-		ft_putendl_fd(error, 2);
-		free(error);
-		free_pathes(env);
-		return (127);
-	}
-}
 
 int	launch_builtin_solo(t_cmd *cmds, char ***env)
 {
@@ -83,19 +38,75 @@ int	launch_builtin_solo(t_cmd *cmds, char ***env)
 	return (ret);
 }
 
+int	launch_builtin(t_cmd *cmds, char ***env, char *path)
+{
+	int	ret;
+
+	free(path);
+	ret = 0;
+	if (cmds->is_builtin == 1)
+		ret = (ft_echo(cmds));
+	else if (cmds->is_builtin == 2)
+		ret = (ft_cd(cmds, *env));
+	else if (cmds->is_builtin == 3)
+		ret = (ft_env(cmds, *env));
+	else if (cmds->is_builtin == 4)
+		ret = (ft_export(cmds, env));
+	else if (cmds->is_builtin == 5)
+		ret = (ft_pwd(cmds));
+	else if (cmds->is_builtin == 6)
+		ret = (ft_unset(cmds, env));
+	else if (cmds->is_builtin == 7)
+		ret = (ft_exit(cmds, *env, 1));
+	free_pathes(*env);
+	return (ret);
+}
+
+int	launch_cmd_next(char *path, char **cmd, char **env)
+{
+	int		ret;
+	char	*error;
+
+	if (path)
+	{
+		execve(path, cmd, env);
+		error = ft_strjoin("minishell: ", path);
+		perror(error);
+		free(error);
+		if (!access(path, F_OK))
+			ret = 126;
+		else
+			ret = 127;
+	}
+	else
+	{
+		error = ft_strjoin(cmd[0], ": command not found");
+		ft_putendl_fd(error, 2);
+		free(error);
+		free_pathes(env);
+		ret = 127;
+	}
+	return (ret);
+}
+
 void	launch_cmd(t_cmd *cmds, int nb_cmds, int pfd[4], char ***env)
 {
 	int		ret;
 	char	*path;
 	char	**pathes;
 	t_cmd	*cmds_cpy;
-
+	
 	cmds_cpy = go_to_cmds(cmds, nb_cmds);
-	pathes = find_pathes(*env);
-	path = check_path(cmds_cpy->cmd[0], pathes);
-	free_pathes(pathes);
+	if (cmds_cpy->cmd)
+	{
+		pathes = find_pathes(*env);
+		path = check_path(cmds_cpy->cmd[0], pathes);
+		free_pathes(pathes);
+	}
 	switch_files(cmds_cpy, nb_cmds, pfd);
 	close_files(cmds, pfd, nb_cmds);
+	if (!cmds_cpy->cmd)
+		exit(0) ;
 	if (cmds_cpy->is_builtin)
 		ret = launch_builtin(cmds_cpy, env, path);
 	else
@@ -104,8 +115,15 @@ void	launch_cmd(t_cmd *cmds, int nb_cmds, int pfd[4], char ***env)
 	exit(ret);
 }
 
-int	check_status(int wstatus)
+int	check_status(t_cmd *cmds)
 {
+	int	wstatus;
+
+	while (cmds)
+	{
+		waitpid(cmds->pid, &wstatus, 0);
+		cmds = cmds->next;
+	}
 	if (WIFEXITED(wstatus))
 		return (WEXITSTATUS(wstatus));
 	else if (WIFSIGNALED(wstatus))
@@ -129,33 +147,28 @@ int	check_status(int wstatus)
 int	executer(t_cmd *cmds, char **env[])
 {
 	int		pfd[4];
-	int		pid;
-	int		wstatus;
 	int		nb_cmds;
 	t_cmd	*cmds_cpy;
 
-	nb_cmds = 0;
 	if (open_files(cmds))
-		return (close_files(cmds, NULL, nb_cmds), 1);
+		return (close_files(cmds, NULL, 1), 1);
 	cmds_cpy = cmds;
 	if (cmds->is_builtin && !cmds->next)
 		return (launch_builtin_solo(cmds, env));
+	nb_cmds = 0;
 	while (cmds_cpy && ++nb_cmds)
 	{
 		if (open_pipe(pfd, nb_cmds, cmds_cpy->next))
 			return (129);
-		pid = try_fork();
-		if (pid < 0)
+		cmds_cpy->pid = try_fork();
+		if (cmds_cpy->pid < 0)
 			return (254);
-		if (pid == 0)
-		{
+		if (cmds_cpy->pid == 0)
 			sig_setup(2);
+		if (cmds_cpy->pid == 0)
 			launch_cmd(cmds, nb_cmds, pfd, env);
-		}
 		cmds_cpy = cmds_cpy->next;
 	}
 	close_files(cmds, pfd, nb_cmds);
-	while (nb_cmds--)
-		waitpid(-1, &wstatus, 0);
-	return (check_status(wstatus));
+	return (check_status(cmds));
 }
--- src/pipe/pipe_utils.c
+++ src/pipe/pipe_utils.c
@@ -6,7 +6,7 @@
 /*   By: aumarin <aumarin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 17:33:45 by aagathe           #+#    #+#             */
-/*   Updated: 2023/10/02 05:33:23 by aagathe          ###   ########.fr       */
+/*   Updated: 2023/10/03 06:39:50 by aagathe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,21 @@ t_cmd	*go_to_cmds(t_cmd *cmds, int nb_cmds)
 	return (cmds);
 }
 
+int	is_path(char *cmd)
+{
+	if (*cmd != '/' && ft_strncmp(cmd, "./", 2) && ft_strncmp(cmd, "../", 3))
+		return (0);
+	return (1);
+}
+
 char	*check_path(char *cmd, char **pathes)
 {
+	char	*ret;
 	char	*path;
 	char	*slash;
 
-	if (ft_strlen(cmd))
+	ret = NULL;
+	if (ft_strlen(cmd) && !is_path(cmd))
 	{
 		while (*pathes)
 		{
@@ -33,12 +42,17 @@ char	*check_path(char *cmd, char **pathes)
 			free(slash);
 			if (!access(path, X_OK))
 				return (path);
+			if (!access(path, F_OK))
+			{
+				free(ret);
+				ret = ft_strdup(path);
+			}
 			free(path);
 		}
 	}
-	if (!access(cmd, X_OK))
-		return (ft_strdup(cmd));
-	return (NULL);
+	else
+		ret = ft_strdup(cmd);
+	return (ret);
 }
 
 char	**find_pathes(char *env[])
